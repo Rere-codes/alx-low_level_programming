ef3d86b4340da20590f1fced485d47e4e39c72e9
Omorere <idowuomorere@gmail.com>
2022-04-12 14:00:38 -0700

    drrf

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - chack for lower case letter
+ * @c : character to be chacked
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
